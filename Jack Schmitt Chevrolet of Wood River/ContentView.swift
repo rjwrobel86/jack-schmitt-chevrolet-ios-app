eec50273bcb8dc7969945dba1125abe46d2299ee
Robert Wrobel <rjwrobel86@gmail.com>
2022-07-13 16:13:10 -0500

    auth0 authentication working!

--- Jack Schmitt Chevrolet of Wood River/ContentView.swift
+++ Jack Schmitt Chevrolet of Wood River/ContentView.swift
@@ -6,13 +6,88 @@
 //
 
 import SwiftUI
-
+import Auth0
 struct ContentView: View {
-    var body: some View {
+  
+  // 1
+  @State private var isAuthenticated = false
+
+
+  var body: some View {
+
+    // 2
+    if isAuthenticated {
+      
+      // 3
+      VStack {
+        
         Menu()
-    }
+        
+        // 4
+        Button("Log out") {
+            logout()
+            
+        }
+        .padding()
+        
+      }
+      
+    } else {
+    
+      // 5
+      VStack {
+        
+        Text("SwiftUI Login Demo")
+          .padding()
+        
+        // 6
+        Button("Log in") {
+          login()
+        }
+        .padding()
+        
+      }
+      
+    } // if isAuthenticated
+    
+  }
+  
 }
 
+extension ContentView {
+      
+    func login() {
+          Auth0 // 1
+            .webAuth() // 2
+            .start { result in // 3
+              switch result {
+                // 4
+                case .failure(let error):
+                  print("Failed with: \(error)")
+                // 5
+                case .success:
+                  self.isAuthenticated = true
+                  print("Credentials = Legit")
+                }
+            }
+        }
+  
+    func logout() {
+       Auth0 // 1
+         .webAuth() // 2
+         .clearSession { result in // 3
+           switch result {
+             // 4
+             case .failure(let error):
+               print("Failed with: \(error)")
+               // 5
+             case .success:
+               self.isAuthenticated = false
+           }
+         }
+     }
+  
+}
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
--- Jack Schmitt Chevrolet of Wood River/Jack_Schmitt_Chevrolet_of_Wood_RiverApp.swift
+++ Jack Schmitt Chevrolet of Wood River/Jack_Schmitt_Chevrolet_of_Wood_RiverApp.swift
@@ -12,22 +12,14 @@ import FirebaseAnalytics
 @main
 struct Jack_Schmitt_Chevrolet_of_Wood_RiverApp: App {
     
-    @UIApplicationDelegateAdaptor(AppDelegate.self) var appDelegate
+    init() {
+        FirebaseApp.configure()
+    }
     
     var body: some Scene {
         WindowGroup {
-            Login()
+            ContentView()
             
         }
-    }
-}
-
-class AppDelegate: NSObject, UIApplicationDelegate {
-    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions:
-                     [UIApplication.LaunchOptionsKey : Any]? = nil) -> Bool {
-        
-        FirebaseApp.configure()
-        
-        return true
     }
 }
